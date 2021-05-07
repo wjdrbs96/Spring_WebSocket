f59ab0c422285e2121bdf3eec0ce04a7cb3d31f2
wjdrbs96 <wjdrbs966@naver.com>
2021-05-08 00:18:57 +0900

    initial commit

---
+++ src/test/java/com/example/springwebsocket/SpringWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebSocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
